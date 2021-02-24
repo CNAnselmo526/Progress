c07186089a29dae67fdae47f8eab476bfe0ca885
Chris Anselmo <chrisanselmo@Chriss-MBP.home>
2021-02-24 15:21:06 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+password = input("What is your password? ")
+if password == "swordfish":
+    print("Access granted. ")
+elif password != "swordfish":
+    print("it swims in the water. Please try again. ")
+    if password != "swordfish":
+        print("Incorrect password. You have one more try.")
+        password = input("What is your password? ")
+        if password == "swordfish":
+            print("Access granted")
+        else:
+            print("You have been locked out.")
+    else:
+        ("Access granted.")
+else:
+    print("Access denied. Please retry password.")
